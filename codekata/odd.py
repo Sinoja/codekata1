2a0a64009872857553097a9fd22b7346fa7086b5
Sinoja <47343595+Sinoja@users.noreply.github.com>
2019-02-05 11:45:43 +0530

    Create odd.py

---
+++ codekata/odd.py
@@ -0,0 +1,8 @@
+num = int(input("enter a number:"))
+flag = num%2
+if flag == 0:
+print(num, "is an even number")
+elif flag == 1:
+print(num, "is an even number")
+else:
+print("Error,Invalid input")
