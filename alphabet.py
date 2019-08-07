9626e7edfabfe81c34e9162419d8e07183319c28
Sinoja <47343595+Sinoja@users.noreply.github.com>
2019-08-07 15:27:36 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
 ch = input("Enter a character: ")
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-    print(ch, "Alphabet")
+    print("Alphabet")
 else:
-    print(ch, "NO")
+    print("NO")
